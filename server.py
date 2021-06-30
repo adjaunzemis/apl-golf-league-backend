cec10acfe242d00d1cc3326495bf3c23a415bd95
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-06-29 22:15:45 -0400

    Updated API post endpoint for course data

--- server.py
+++ server.py
@@ -7,13 +7,12 @@ Andris Jaunzemis
 
 """
 
-from os import path
-from datetime import datetime
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 
 from apl_golf_league_database import APLGolfLeagueDatabase
+from golf_course import GolfCourse
 
 class Courses(Resource):
     def get(self):
@@ -26,10 +25,17 @@ class Courses(Resource):
         name = args['name']
 
         db = APLGolfLeagueDatabase('./config/admin.user', verbose=False)
-        courses = db.get_courses(id=id, name=name, verbose=True)
+        courses = db.get_courses(id=id, name=name, verbose=False)
         if courses is None:
             return []
         return [course.as_dict() for course in courses]
+
+    def post(self):
+        course_data = request.get_json(force=True)
+        course = GolfCourse.from_dict(course_data)
+
+        db = APLGolfLeagueDatabase('./config/admin.user', verbose=False)
+        db.put_course(course, verbose=True)
         
 class APLGolfLeagueServer(object):
 
@@ -47,5 +53,4 @@ class APLGolfLeagueServer(object):
         self._app.run(port=port, debug=debug)
 
 if __name__ == "__main__":
-    server = APLGolfLeagueServer(debug=True)
-    server.run(port=3000, debug=False)
+    server = APLGolfLeagueServer(port=3000, debug=False)
