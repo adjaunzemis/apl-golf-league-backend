ca978606c50ce902bc9dcb6a01599ac5a0e6563a
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-10-05 21:34:57 -0400

    Added placeholder metadata to API

--- app/main.py
+++ app/main.py
@@ -3,7 +3,19 @@ from fastapi import FastAPI
 from .dependencies import create_db_and_tables
 from .routers import courses, golfers
 
-app = FastAPI()
+description = """
+APLGolfLeague API
+"""
+
+app = FastAPI(
+    title="APLGolfLeague",
+    description=description,
+    version="0.1.0",
+    contact={
+        "name": "Andris Jaunzemis",
+        "email": "adjaunzemis@gmail.com",
+    }
+)
 
 app.include_router(courses.router)
 app.include_router(golfers.router)
