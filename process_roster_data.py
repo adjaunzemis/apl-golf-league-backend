e938e862c4557c01774ca6cdc3bc4b5c97141a26
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-10-09 21:50:06 -0400

    Updated old roster file processing script

--- process_roster_data.py
+++ process_roster_data.py
@@ -1,11 +1,18 @@
 r"""
-Processing script for roster data files
+Processing script for old roster data files.
+
+Creates CSV files to support populating the following tables:
+- golfer: name, affiliation
+- team: name, flight_id
+- player: team_id, golfer_id, division_id, role
 
 Authors
 -------
 Andris Jaunzemis
 
 """
+
+import os
 
 def parse_roster_data_from_file(file):
     players = []
@@ -20,7 +27,7 @@ def parse_roster_data_from_file(file):
                 currentCourse = " ".join(line.split()[1:])
             elif line[:6].lower() == 'golfer':
                 # Parse player information
-                #   GOLFER <first_name> <last_name> <location> <phone> <?> <email> <tees> <classification> <employee_id>
+                #   GOLFER <first_name> <last_name> <location> <phone> <?> <email> <tees> <affiliation> <employee_id>
                 line_parts = line.split()
                 if len(line_parts) != 10:
                     raise ValueError("Unable to parse roster player line: {:s}".format(line))
@@ -29,32 +36,43 @@ def parse_roster_data_from_file(file):
                     'name': " ".join(line_parts[1:3]),
                     'team': currentTeam,
                     'course': currentCourse,
-                    'location': line_parts[3],
+                    'office': line_parts[3],
                     'phone': line_parts[4],
                     'other': line_parts[5],
                     'email': line_parts[6],
-                    'tees': line_parts[7],
-                    'classification': line_parts[8],
+                    'division': line_parts[7],
+                    'affiliation': line_parts[8],
                     'employee_id': line_parts[9]
                 }
                 players.append(player)
     return players
 
 if __name__ == "__main__":
-    DATA_YEAR = 2019
-    ROSTER_DATA_FILES = ["roster.dr.data", "roster.fh.data", "roster.nwp.data", "roster.rw.data", "roster.tat.data", "roster.playoffs.data"]
+    data_dirs = [f for f in os.listdir("data/") if f[0:10] == "golf_data."]
+    for data_dir in data_dirs:
+        data_year = 2000 + int(data_dir[-2:])
 
-    for roster_data_file in ROSTER_DATA_FILES:
-        print("Processing {:d} roster data file: {:s}".format(DATA_YEAR, roster_data_file))
+        roster_files = [f for f in os.listdir(f"data/{data_dir}") if f[0:7] == "roster."]
+        for roster_file in roster_files:
+            print(f"Processing {data_year} roster file: {roster_file}")
 
-        roster_dict_list = parse_roster_data_from_file("data/{:s}".format(roster_data_file))
+            flight_name = roster_file.split(".")[1]
+            output_file = f"data/roster_{data_year}_{flight_name}.csv"
 
-        csv_data = ",".join([str(k) for k,v in roster_dict_list[0].items()])
-        for match_dict in roster_dict_list:
-            csv_data += "\n" + ",".join([str(v) for k,v in match_dict.items()])
+            roster_dict_list = []
+            try:
+                roster_dict_list = parse_roster_data_from_file(f"data/{data_dir}/{roster_file}")
+            except ValueError:
+                print("\tERROR: Unable to process roster file!")
+            
+            if len(roster_dict_list) == 0:
+                print("\tNo roster data to output")
+            else:
+                csv_data = ",".join([str(k) for k,v in roster_dict_list[0].items()])
+                for roster_dict in roster_dict_list:
+                    csv_data += "\n" + ",".join([str(v) for k,v in roster_dict.items()])
 
-        course_abbreviation = roster_data_file.split(".")[1]
-        outputFile = "data/roster_{:s}_{:d}.csv".format(roster_data_file.split(".")[1], DATA_YEAR)
-        with open(outputFile, "w") as fp:
-            fp.write(csv_data)
+                print(f"\tWriting processed data to file: {output_file}")
+                with open(output_file, "w") as fp:
+                    fp.write(csv_data)
         
