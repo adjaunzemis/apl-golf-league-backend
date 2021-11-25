7fd94af2b2bfdbd33941066aea1141ba8b6b992b
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-11-24 23:06:36 -0500

    Implemented legacy APL golf league handicap system
    Added unit test suite for legacy handicap system

---
+++ app/utilities/apl_legacy_handicap_system.py
@@ -0,0 +1,63 @@
+from typing import List
+import numpy as np
+
+from .world_handicap_system import WorldHandicapSystem
+
+class APLLegacyHandicapSystem(WorldHandicapSystem):
+    """
+    Legacy implementation of the APL golf league handicap system.
+
+    Similar to USGA/WHS with some adjustments for 9-hole league play.
+    
+    All score differentials are computed over 9-hole rounds, so the handicap
+    index is a 9-hole handicap index.
+
+    Handicap index calculation uses fewer scores from scoring record, which is
+    the latest 10 score differentials.
+
+    For maximum score per hole, uses pre-2020 USGA equitable stroke control.
+
+    References:
+    - APL Golf League Handicapping: http://aplgolfleague.com/APL_Golf/handicap.html
+
+    """
+
+    def compute_hole_maximum_score(self, par: int, stroke_index: int, course_handicap: int = None) -> int:
+        # Reference: USGA Equitable Stroke Control (prior to 2020)
+        if course_handicap <= 4:
+            return par + 2
+        elif course_handicap <= 9:
+            return 7
+        elif course_handicap <= 14:
+            return 8
+        elif course_handicap <= 19:
+            return 9
+        else:
+            return 10
+
+    def compute_hole_handicap_strokes(self, stroke_index: int, course_handicap: int) -> int:
+        # Similar to WHS, but using 9-hole handicaps only and assuming hole
+        # stroke index is normalized to 1-to-9 range (not 1-to-18).
+        if course_handicap < 0: # plus-handicap
+            return -(int(-course_handicap / 9) + int(-course_handicap % 9 >= stroke_index))
+        return int(course_handicap / 9) + int(course_handicap % 9 >= stroke_index)
+    
+    def compute_handicap_index(self, record: List[float]) -> float:
+        # Reference: APL Golf League Handicapping
+        record_sorted = np.sort(record)
+        if len(record) < 4:
+            score_diffs_avg = record_sorted[0]
+        elif len(record) < 6:
+            score_diffs_avg = np.mean(record_sorted[0:2])
+        elif len(record) < 8:
+            score_diffs_avg = np.mean(record_sorted[0:3])
+        elif len(record) < 10:
+            score_diffs_avg = np.mean(record_sorted[0:4])
+        else:
+            score_diffs_avg = np.mean(record_sorted[0:5])
+        return np.floor((0.96 * score_diffs_avg) * 10.0) / 10.0 # truncate to nearest tenth
+
+    @property
+    def maximum_handicap_index(self) -> float:
+        # Reference: APL Golf League Handicapping
+        return 30.0
---
+++ app/utilities/test_apl_legacy_handicap_system.py
@@ -0,0 +1,99 @@
+import pytest
+
+from .apl_legacy_handicap_system import APLLegacyHandicapSystem
+
+@pytest.mark.parametrize(
+    "par, stroke_index, course_handicap, max_score", [
+        (4, 7, 30, 10),
+        (4, 7, 25, 10),
+        (4, 7, 20, 10),
+        (4, 7, 19, 9),
+        (4, 7, 15, 9),
+        (4, 7, 14, 8),
+        (4, 7, 10, 8),
+        (4, 7, 9, 7),
+        (4, 7, 5, 7),
+        (4, 7, 4, 6),
+        (4, 7, 0, 6),
+        (4, 7, -1, 6),
+        (4, 1, 4, 6),
+        (4, 9, 4, 6),
+        (5, 9, 4, 7),
+        (3, 9, 4, 5),
+    ])
+def test_compute_hole_maximum_score(par, stroke_index, course_handicap, max_score):
+    alhs = APLLegacyHandicapSystem()
+    assert alhs.compute_hole_maximum_score(par, stroke_index, course_handicap) == max_score
+
+@pytest.mark.parametrize(
+    "stroke_index, course_handicap, handicap_strokes", [
+        (5, 30, 3),
+        (5, 23, 3),
+        (5, 20, 2),
+        (5, 14, 2),
+        (5, 10, 1),
+        (5, 5, 1),
+        (5, 3, 0),
+        (5, 0, 0),
+        (5, -2, 0),
+        (5, -5, -1),
+        (5, -9, -1),
+        (5, -14, -2)
+    ])
+def test_compute_hole_handicap_strokes(stroke_index, course_handicap, handicap_strokes):
+    alhs = APLLegacyHandicapSystem()
+    assert alhs.compute_hole_handicap_strokes(stroke_index, course_handicap) == handicap_strokes
+
+@pytest.mark.parametrize(
+    "par, stroke_index, score, course_handicap, adjusted_score", [
+        (4, 7, 8, 16, 8),
+        (4, 7, 11, 20, 10),
+        (4, 7, 11, 15, 9),
+        (4, 7, 11, 10, 8),
+        (4, 7, 11, 5, 7),
+        (4, 7, 11, 4, 6),
+        (5, 7, 11, 15, 9),
+        (5, 7, 11, 2, 7),
+        (4, 1, 11, 1, 6),
+        (4, 9, 11, 1, 6)
+    ])
+def test_compute_hole_adjusted_gross_score(par, stroke_index, score, course_handicap, adjusted_score):
+    alhs = APLLegacyHandicapSystem()
+    assert alhs.compute_hole_adjusted_gross_score(par, stroke_index, score, course_handicap=course_handicap) == adjusted_score
+    
+@pytest.mark.parametrize(
+    "course_par, course_rating, course_slope, handicap_index, course_handicap", [
+        (72, 73.1, 132, 12, 15.12)
+    ])
+def test_compute_course_handicap(course_par, course_rating, course_slope, handicap_index, course_handicap):
+    alhs = APLLegacyHandicapSystem()
+    assert pytest.approx(alhs.compute_course_handicap(course_par, course_rating, course_slope, handicap_index), abs=1e-2) == course_handicap
+
+@pytest.mark.parametrize(
+    "course_rating, course_slope, adj_gross_score, pcc, score_diff", [
+        (70.9, 121, 83, 0, 11.3),
+        (70.9, 121, 82, 0, 10.4),
+        (68.7, 115, 92, 0, 22.9),
+        (68.7, 115, 92, -0.5, 23.4),
+        (68.7, 115, 92, 1.5, 21.4),
+        (71.1, 124, 73, 0, 1.7),
+        (71.1, 122, 71, 0, -0.1),
+        (71.1, 122, 68, 0, -2.9),
+        (70.6, 133, 68, 0, -2.2)
+    ])
+def test_compute_score_differential(course_rating, course_slope, adj_gross_score, pcc, score_diff):
+    alhs = APLLegacyHandicapSystem()
+    assert alhs.compute_score_differential(course_rating, course_slope, adj_gross_score, pcc) == score_diff
+
+@pytest.mark.parametrize(
+    "records, handicap_index", [
+        ([4.5, 5.6, 3.4, 8.7, 6.8, 10.2, 4.3, 8.8, 6.2, 4.8], 4.3),
+        ([13.4, 16.7, 21.2, 13.9], 13.1)
+    ])
+def test_compoute_handicap_index(records, handicap_index):
+    alhs = APLLegacyHandicapSystem()
+    assert alhs.compute_handicap_index(records) == handicap_index
+
+def test_maximum_handicap_index():
+    alhs = APLLegacyHandicapSystem()
+    assert alhs.maximum_handicap_index == 30.0
