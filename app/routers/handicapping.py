dcafeb487178920e2910a51f567003169799477b
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-01-17 17:35:42 -0500

    Added handicap index route

--- app/routers/handicapping.py
+++ app/routers/handicapping.py
@@ -1,5 +1,5 @@
 from typing import List
-from datetime import date
+from datetime import date, timezone, datetime
 from fastapi import APIRouter, Depends, Query
 from fastapi.exceptions import HTTPException
 from sqlmodel import SQLModel, Session, select
@@ -23,10 +23,27 @@ class ScoringRecordEntry(SQLModel):
     gross_score: int
     adjusted_gross_score: int
 
+class HandicapIndexWithScoringRecord(SQLModel):
+    handicap_index: float
+    date: date
+    scoring_record: List[ScoringRecordEntry] = []
+
 # TODO: Add score differential to round data on creation or import to database
-@router.get("/scoring-record/id={golfer_id}&date={date}", response_model=List[ScoringRecordEntry])
-async def get_scoring_record(*, session: Session = Depends(get_session), golfer_id: int, date: date):
-    rounds = get_rounds_in_scoring_record(session=session, golfer_id=golfer_id, date=date, limit=10)
+def create_scoring_record_from_rounds(rounds: List[RoundData]) -> List[ScoringRecordEntry]:
+    """
+    Extracts scoring record data from round data.
+
+    Parameters
+    ----------
+    rounds : list of RoundData
+        round data
+
+    Returns
+    -------
+    record : list of ScoringRecordEntry
+        scoring record
+
+    """
     ahs = APLLegacyHandicapSystem()
     return [ScoringRecordEntry(
         date=r.date_played,
@@ -35,3 +52,18 @@ async def get_scoring_record(*, session: Session = Depends(get_session), golfer_
         slope=r.tee_slope,
         gross_score=r.gross_score,
         adjusted_gross_score=r.adjusted_gross_score) for r in rounds]
+
+@router.get("/scoring-record/id={golfer_id}", response_model=List[ScoringRecordEntry])
+async def get_scoring_record(*, session: Session = Depends(get_session), golfer_id: int, date: date = Query(default=date.today())):
+    rounds = get_rounds_in_scoring_record(session=session, golfer_id=golfer_id, date=date, limit=10)
+    return create_scoring_record_from_rounds(rounds=rounds)
+
+@router.get("/handicap-index/id={golfer_id}", response_model=HandicapIndexWithScoringRecord)
+async def get_handicap_index(*, session: Session = Depends(get_session), golfer_id: int = Query(default=None), date: date = Query(default=date.today()), include_record: bool = Query(default=False)):
+    ahs = APLLegacyHandicapSystem()
+    rounds = get_rounds_in_scoring_record(session=session, golfer_id=golfer_id, date=date, limit=10)
+    record = [ahs.compute_score_differential(r.tee_rating, r.tee_slope, r.adjusted_gross_score) for r in rounds]
+    response = HandicapIndexWithScoringRecord(date=date, handicap_index=ahs.compute_handicap_index(record=record))
+    if include_record:
+        response.scoring_record = create_scoring_record_from_rounds(rounds=rounds)
+    return response
