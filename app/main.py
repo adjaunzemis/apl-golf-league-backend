a04c7862f965247754ae5f3ce79de77f3f807107
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-02-17 07:53:28 -0500

    Added loguru logging for request details

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum
 
-from .dependencies import create_db_and_tables
+from .dependencies import create_db_and_tables, log_request_data
 from .routers import courses, golfers, teams, flights, tournaments, divisions, rounds, matches, handicapping, officers
 
 description = """
@@ -27,19 +27,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(courses.router)
-app.include_router(golfers.router)
-app.include_router(teams.router)
-app.include_router(flights.router)
-app.include_router(tournaments.router)
-app.include_router(divisions.router)
-app.include_router(rounds.router)
-app.include_router(matches.router)
-app.include_router(officers.router)
-app.include_router(handicapping.router)
+app.include_router(courses.router, dependencies=[Depends(log_request_data)])
+app.include_router(golfers.router, dependencies=[Depends(log_request_data)])
+app.include_router(teams.router, dependencies=[Depends(log_request_data)])
+app.include_router(flights.router, dependencies=[Depends(log_request_data)])
+app.include_router(tournaments.router, dependencies=[Depends(log_request_data)])
+app.include_router(divisions.router, dependencies=[Depends(log_request_data)])
+app.include_router(rounds.router, dependencies=[Depends(log_request_data)])
+app.include_router(matches.router, dependencies=[Depends(log_request_data)])
+app.include_router(officers.router, dependencies=[Depends(log_request_data)])
+app.include_router(handicapping.router, dependencies=[Depends(log_request_data)])
 
 @app.get('/')
-def hello_world():
+def hello_world(log = Depends(log_request_data)):
     return {"message": "Hello from APLGolfLeagueAPI!"}
 
 @app.on_event("startup")
