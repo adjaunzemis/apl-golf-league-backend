68b397d1d9d2972d25c8b21d932f538e4bf195dc
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-04-10 11:42:05 -0400

    Incremented version number to reflect patch updates

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ APL Golf League API
 app = FastAPI(
     title="APL Golf League",
     description=description,
-    version="0.2.0",
+    version="0.2.1",
     contact={
         "name": "Andris Jaunzemis",
         "email": "adjaunzemis@gmail.com",
