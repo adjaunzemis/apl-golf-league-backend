88e5581c00e635938450c961f77f42eb3f68d059
Andris Jaunzemis <adjaunzemis@gmail.com>
2022-07-31 17:57:32 -0400

    Updated active handicap date range

--- app/update_golfer_handicaps.py
+++ app/update_golfer_handicaps.py
@@ -322,5 +322,5 @@ if __name__ == "__main__":
 
     with Session(engine) as session:
         # recalculate_hole_results(session=session, year=2022)
-        update_golfer_handicaps(session=session, old_max_date=datetime(2022, 7, 11), new_max_date=datetime(2022, 7, 18)) # TODO: un-hardcode dates
+        update_golfer_handicaps(session=session, old_max_date=datetime(2022, 7, 25), new_max_date=datetime(2022, 8, 1)) # TODO: un-hardcode dates
     print('Done!')
