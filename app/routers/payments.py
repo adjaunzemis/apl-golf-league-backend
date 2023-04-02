ff3e1da0500cc6ee5045d1f830c841aa53e94d8c
Andris Jaunzemis <adjaunzemis@gmail.com>
2023-04-02 15:28:34 -0400

    Refactored payments service into data/info routes

--- app/routers/payments.py
+++ app/routers/payments.py
@@ -1,13 +1,14 @@
 from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException
 from http import HTTPStatus
-from sqlmodel import Session, select
+from sqlmodel import SQLModel, Session, select
 
 from ..dependencies import get_current_active_user, get_sql_db_session
 from ..models.payment import (
     LeagueDuesPayment,
     LeagueDuesPaymentRead,
     LeagueDuesPaymentUpdate,
+    LeagueDuesType,
 )
 from ..models.user import User
 from ..models.golfer import Golfer
@@ -15,13 +16,48 @@ from ..models.golfer import Golfer
 router = APIRouter(prefix="/payments", tags=["Payments"])
 
 
-class LeagueDuesPaymentInfo(LeagueDuesPaymentRead):
+class LeagueDuesPaymentData(LeagueDuesPaymentRead):
     golfer_name: str
     golfer_email: Optional[str]
 
 
-@router.get("/", response_model=List[LeagueDuesPaymentInfo])
-async def read_league_dues_payments_for_year(
+class LeagueDuesPaymentInfo(SQLModel):
+    id: int
+    golfer_id: int
+    golfer_name: str
+    year: int
+    type: LeagueDuesType
+    amount_due: float
+    amount_paid: float
+    is_paid: bool
+
+
+@router.get("/dues/info", response_model=List[LeagueDuesPaymentInfo])
+async def read_league_dues_payment_info_for_year(
+    *, session: Session = Depends(get_sql_db_session), year: int
+):
+    payment_query_data = session.exec(
+        select(LeagueDuesPayment, Golfer)
+        .join(Golfer, onclause=Golfer.id == LeagueDuesPayment.golfer_id)
+        .where(LeagueDuesPayment.year == year)
+    ).all()
+    return [
+        LeagueDuesPaymentInfo(
+            id=payment_db.id,
+            golfer_id=payment_db.golfer_id,
+            golfer_name=golfer_db.name,
+            year=payment_db.year,
+            type=payment_db.type,
+            amount_due=payment_db.amount_due,
+            amount_paid=payment_db.amount_paid,
+            is_paid=payment_db.is_paid,
+        )
+        for payment_db, golfer_db in payment_query_data
+    ]
+
+
+@router.get("/dues/data", response_model=List[LeagueDuesPaymentData])
+async def read_league_dues_payment_data_for_year(
     *,
     session: Session = Depends(get_sql_db_session),
     current_user: User = Depends(get_current_active_user),
@@ -38,7 +74,7 @@ async def read_league_dues_payments_for_year(
         .where(LeagueDuesPayment.year == year)
     ).all()
     return [
-        LeagueDuesPaymentInfo(
+        LeagueDuesPaymentData(
             id=payment_db.id,
             golfer_id=payment_db.golfer_id,
             golfer_name=golfer_db.name,
