f240cd7b009998451779e3515e8bd4990b7a2af6
Andris Jaunzemis <adjaunzemis@gmail.com>
2024-02-05 23:17:32 -0500

    Started courses router test updates

--- tests/routers/test_courses.py
+++ tests/routers/test_courses.py
@@ -1,15 +1,21 @@
 import pytest
+from fastapi import status
 from fastapi.testclient import TestClient
 from sqlmodel import Session, SQLModel, create_engine
 from sqlmodel.pool import StaticPool
 
 from app.api import app
-from app.dependencies import get_sql_db_session
+from app.dependencies import get_sql_db_session, get_current_user
 from app.models.course import Course
 from app.models.track import Track
-from app.models.tee import Tee
+from app.models.tee import Tee, TeeGender
 from app.models.hole import Hole
+from app.models.user import User
 
+async def override_get_current_user():
+    return User(username="test_user", is_admin=True, disabled=False)
+
+app.dependency_overrides[get_current_user] = override_get_current_user
 
 @pytest.fixture(name="session")
 def session_fixture():
@@ -62,6 +68,8 @@ def client_fixture(session: Session):
 def test_create_course(
     client: TestClient, name: str, year: int, address: str, phone: str, website: str
 ):
+    app.dependency_overrides[get_current_user] = override_get_current_user # force admin user
+
     response = client.post(
         "/courses/",
         json={
@@ -72,7 +80,7 @@ def test_create_course(
             "website": website,
         },
     )
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert data["name"] == name
@@ -80,7 +88,21 @@ def test_create_course(
     assert data["phone"] == phone
     assert data["website"] == website
     assert data["id"] is not None
+    
+    app.dependency_overrides = {} # remove dependency overrides
 
+def test_create_course_unauthorized(client: TestClient):
+    response = client.post(
+        "/courses/",
+        json={
+            "name": "Test Course",
+            "year": 2021,
+            "address": "Test Street, Test City, ST 12345",
+            "phone": "123-456-7890",
+            "website": "google.com",
+        }
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 @pytest.mark.parametrize(
     "name, year, address, phone, website",
@@ -98,7 +120,8 @@ def test_create_course(
 def test_create_course_incomplete(
     client: TestClient, name: str, year: int, address: str, phone: str, website: str
 ):
-    # Missing required fieds
+    app.dependency_overrides[get_current_user] = override_get_current_user # force admin user
+    
     response = client.post(
         "/courses/",
         json={
@@ -109,7 +132,9 @@ def test_create_course_incomplete(
             "website": website,
         },
     )
-    assert response.status_code == 422
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    
+    app.dependency_overrides = {} # remove dependency overrides
 
 
 @pytest.mark.parametrize(
@@ -149,12 +174,15 @@ def test_create_course_incomplete(
 def test_create_course_invalid(
     client: TestClient, name: str, year: int, address: str, phone: str, website: str
 ):
-    # Invalid input data types
+    app.dependency_overrides[get_current_user] = override_get_current_user # force admin user
+
     response = client.post(
         "/courses/",
         json={"name": name, "address": address, "phone": phone, "website": website},
     )
-    assert response.status_code == 422
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    
+    app.dependency_overrides = {} # remove dependency overrides
 
 
 def test_read_courses(session: Session, client: TestClient):
@@ -179,7 +207,7 @@ def test_read_courses(session: Session, client: TestClient):
     session.commit()
 
     response = client.get("/courses/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert len(data) == len(courses)
@@ -204,7 +232,7 @@ def test_read_course(session: Session, client: TestClient):
     session.commit()
 
     response = client.get(f"/courses/{course.id}")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert data["name"] == course.name
@@ -216,30 +244,9 @@ def test_read_course(session: Session, client: TestClient):
     assert len(data["tracks"]) == 0
 
 
-def test_update_course(session: Session, client: TestClient):
-    course = Course(
-        name="Test Course 1",
-        year=2021,
-        address="Test Address 1",
-        phone="111-111-1111",
-        website="google.com",
-    )
-    session.add(course)
-    session.commit()
-
-    response = client.patch(f"/courses/{course.id}", json={"name": "Awesome Course"})
-    assert response.status_code == 200
-
-    data = response.json()
-    assert data["name"] == "Awesome Course"
-    assert data["year"] == course.year
-    assert data["address"] == course.address
-    assert data["phone"] == course.phone
-    assert data["website"] == course.website
-    assert data["id"] == course.id
-
-
 def test_delete_course(session: Session, client: TestClient):
+    app.dependency_overrides[get_current_user] = override_get_current_user
+
     course = Course(
         name="Test Course 1",
         year=2021,
@@ -251,42 +258,12 @@ def test_delete_course(session: Session, client: TestClient):
     session.commit()
 
     response = client.delete(f"/courses/{course.id}")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     course_db = session.get(Course, course.id)
     assert course_db is None
-
-
-@pytest.mark.parametrize(
-    "name, course_id", [("Test Track Name", 1), ("Test Track Name", None)]
-)
-def test_create_track(client: TestClient, name: str, course_id: int):
-    response = client.post(
-        "/courses/tracks/", json={"name": name, "course_id": course_id}
-    )
-    assert response.status_code == 200
-
-    data = response.json()
-    assert data["name"] == name
-    assert data["course_id"] == course_id
-    assert data["id"] is not None
-
-
-def test_create_track_incomplete(client: TestClient):
-    # Missing required 'name' field
-    response = client.post("/courses/tracks/", json={"course_id": 1})
-    assert response.status_code == 422
-
-
-@pytest.mark.parametrize(
-    "name, course_id", [({"key": "value"}, 1), ("Test Track Name", {"key": "value"})]
-)
-def test_create_track_invalid(client: TestClient, name: str, course_id: int):
-    # Invalid input data types
-    response = client.post(
-        "/courses/tracks/", json={"name": name, "course_id": course_id}
-    )
-    assert response.status_code == 422
+    
+    app.dependency_overrides = {} # remove dependency overrides
 
 
 def test_read_tracks(session: Session, client: TestClient):
@@ -299,7 +276,7 @@ def test_read_tracks(session: Session, client: TestClient):
     session.commit()
 
     response = client.get("/courses/tracks/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert len(data) == len(tracks)
@@ -315,7 +292,7 @@ def test_read_track(session: Session, client: TestClient):
     session.commit()
 
     response = client.get(f"/courses/tracks/{track.id}")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert data["name"] == track.name
@@ -332,7 +309,7 @@ def test_update_track(session: Session, client: TestClient):
     response = client.patch(
         f"/courses/tracks/{track.id}", json={"name": "Awesome Track"}
     )
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert data["name"] == "Awesome Track"
@@ -341,131 +318,25 @@ def test_update_track(session: Session, client: TestClient):
 
 
 def test_delete_track(session: Session, client: TestClient):
+    app.dependency_overrides[get_current_user] = override_get_current_user
+
     track = Track(name="Test Track 1", course_id=1)
     session.add(track)
     session.commit()
 
     response = client.delete(f"/courses/tracks/{track.id}")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     track_db = session.get(Track, track.id)
     assert track_db is None
 
-
-@pytest.mark.parametrize(
-    "name, gender, rating, slope, color, track_id",
-    [
-        ("Test Tee Name", "M", 72.3, 128, "Blue", 1),
-        ("Test Tee Name", "F", 72.3, 128, "Blue", None),
-        ("Test Tee Name", "M", 72.3, 128, None, 1),
-    ],
-)
-def test_create_tee(
-    client: TestClient,
-    name: str,
-    gender: str,
-    rating: float,
-    slope: int,
-    color: str,
-    track_id: int,
-):
-    response = client.post(
-        "/courses/tees/",
-        json={
-            "name": name,
-            "gender": gender,
-            "rating": rating,
-            "slope": slope,
-            "color": color,
-            "track_id": track_id,
-        },
-    )
-    assert response.status_code == 200
-
-    data = response.json()
-    assert data["name"] == name
-    assert data["gender"] == gender
-    assert data["rating"] == rating
-    assert data["slope"] == slope
-    assert data["color"] == color
-    assert data["track_id"] == track_id
-    assert data["id"] is not None
-
-
-@pytest.mark.parametrize(
-    "name, gender, rating, slope, color, track_id",
-    [
-        (None, "M", 72.3, 128, "Blue", 1),
-        ("Test Tee Name", None, 72.3, 128, "Blue", 1),
-        ("Test Tee Name", "M", None, 128, "Blue", 1),
-        ("Test Tee Name", "M", 72.3, None, "Blue", 1),
-    ],
-)
-def test_create_tee_incomplete(
-    client: TestClient,
-    name: str,
-    gender: str,
-    rating: float,
-    slope: int,
-    color: str,
-    track_id: int,
-):
-    # Missing required fields
-    response = client.post(
-        "/courses/tees/",
-        json={
-            "name": name,
-            "gender": gender,
-            "rating": rating,
-            "slope": slope,
-            "color": color,
-            "track_id": track_id,
-        },
-    )
-    assert response.status_code == 422
-
-
-@pytest.mark.parametrize(
-    "name, gender, rating, slope, color, track_id",
-    [
-        ({"key": "value"}, "M", 72.3, 128, "Blue", 1),
-        ("Test Tee Name", {"key": "value"}, 72.3, 128, "Blue", 1),
-        ("Test Tee Name", "M", {"key": "value"}, 128, "Blue", 1),
-        ("Test Tee Name", "M", 72.3, {"key": "value"}, "Blue", 1),
-        ("Test Tee Name", "M", 72.3, 128, {"key": "value"}, 1),
-        ("Test Tee Name", "M", 72.3, 128, "Blue", {"key": "value"}),
-        ("Test Tee Name", "INVALID GENDER", 72.3, 128, "Blue", 1),
-    ],
-)
-def test_create_tee_invalid(
-    client: TestClient,
-    name: str,
-    gender: str,
-    rating: float,
-    slope: int,
-    color: str,
-    track_id: int,
-):
-    # Invalid input data types
-    response = client.post(
-        "/courses/tees/",
-        json={
-            "name": name,
-            "gender": gender,
-            "rating": rating,
-            "slope": slope,
-            "color": color,
-            "track_id": track_id,
-        },
-    )
-    assert response.status_code == 422
-
+    app.dependency_overrides = {} # remove dependency overrides
 
 def test_read_tees(session: Session, client: TestClient):
     tees = [
         Tee(
             name="Test Tee 1",
-            gender="M",
+            gender=TeeGender.MENS,
             rating=72.3,
             slope=128,
             color="Blue",
@@ -473,7 +344,7 @@ def test_read_tees(session: Session, client: TestClient):
         ),
         Tee(
             name="Test Tee 2",
-            gender="F",
+            gender=TeeGender.LADIES,
             rating=68.7,
             slope=117,
             color="Red",
@@ -485,7 +356,7 @@ def test_read_tees(session: Session, client: TestClient):
     session.commit()
 
     response = client.get("/courses/tees/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert len(data) == len(tees)
@@ -501,13 +372,13 @@ def test_read_tees(session: Session, client: TestClient):
 
 def test_read_tee(session: Session, client: TestClient):
     tee = Tee(
-        name="Test Tee 1", gender="M", rating=72.3, slope=128, color="Blue", track_id=1
+        name="Test Tee 1", gender=TeeGender.MENS, rating=72.3, slope=128, color="Blue", track_id=1
     )
     session.add(tee)
     session.commit()
 
     response = client.get(f"/courses/tees/{tee.id}")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert data["name"] == tee.name
@@ -522,13 +393,13 @@ def test_read_tee(session: Session, client: TestClient):
 
 def test_update_tee(session: Session, client: TestClient):
     tee = Tee(
-        name="Test Tee 1", gender="M", rating=72.3, slope=128, color="Blue", track_id=1
+        name="Test Tee 1", gender=TeeGender.MENS, rating=72.3, slope=128, color="Blue", track_id=1
     )
     session.add(tee)
     session.commit()
 
     response = client.patch(f"/courses/tees/{tee.id}", json={"name": "Awesome Tee"})
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert data["name"] == "Awesome Tee"
@@ -542,112 +413,16 @@ def test_update_tee(session: Session, client: TestClient):
 
 def test_delete_tee(session: Session, client: TestClient):
     tee = Tee(
-        name="Test Tee 1", gender="M", rating=72.3, slope=128, color="Blue", track_id=1
+        name="Test Tee 1", gender=TeeGender.MENS, rating=72.3, slope=128, color="Blue", track_id=1
     )
     session.add(tee)
     session.commit()
 
     response = client.delete(f"/courses/tees/{tee.id}")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     tee_db = session.get(Tee, tee.id)
     assert tee_db is None
-
-
-@pytest.mark.parametrize(
-    "number, par, yardage, stroke_index, tee_id",
-    [(1, 4, 385, 3, 1), (1, 4, 385, 3, None)],
-)
-def test_create_hole(
-    client: TestClient,
-    number: int,
-    par: int,
-    yardage: int,
-    stroke_index: int,
-    tee_id: int,
-):
-    response = client.post(
-        "/courses/holes/",
-        json={
-            "number": number,
-            "par": par,
-            "yardage": yardage,
-            "stroke_index": stroke_index,
-            "tee_id": tee_id,
-        },
-    )
-    assert response.status_code == 200
-
-    data = response.json()
-    assert data["number"] == number
-    assert data["par"] == par
-    assert data["yardage"] == yardage
-    assert data["stroke_index"] == stroke_index
-    assert data["tee_id"] == tee_id
-    assert data["id"] is not None
-
-
-@pytest.mark.parametrize(
-    "number, par, yardage, stroke_index, tee_id",
-    [
-        (None, 4, 385, 3, 1),
-        (1, None, 385, 3, 1),
-        (1, 4, None, 3, 1),
-        (1, 4, 385, None, 1),
-    ],
-)
-def test_create_hole_incomplete(
-    client: TestClient,
-    number: int,
-    par: int,
-    yardage: int,
-    stroke_index: int,
-    tee_id: int,
-):
-    # Missing required fields
-    response = client.post(
-        "/courses/holes/",
-        json={
-            "number": number,
-            "par": par,
-            "yardage": yardage,
-            "stroke_index": stroke_index,
-            "tee_id": tee_id,
-        },
-    )
-    assert response.status_code == 422
-
-
-@pytest.mark.parametrize(
-    "number, par, yardage, stroke_index, tee_id",
-    [
-        ({"key": "value"}, 4, 385, 3, 1),
-        (1, {"key": "value"}, 385, 3, 1),
-        (1, 4, {"key": "value"}, 3, 1),
-        (1, 4, 385, {"key": "value"}, 1),
-        (1, 4, 385, 3, {"key": "value"}),
-    ],
-)
-def test_create_hole_invalid(
-    client: TestClient,
-    number: int,
-    par: int,
-    yardage: int,
-    stroke_index: int,
-    tee_id: int,
-):
-    # Invalid input data types
-    response = client.post(
-        "/courses/holes/",
-        json={
-            "number": number,
-            "par": par,
-            "yardage": yardage,
-            "stroke_index": stroke_index,
-            "tee_id": tee_id,
-        },
-    )
-    assert response.status_code == 422
 
 
 def test_read_holes(session: Session, client: TestClient):
@@ -660,7 +435,7 @@ def test_read_holes(session: Session, client: TestClient):
     session.commit()
 
     response = client.get("/courses/holes/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert len(data) == len(holes)
@@ -679,7 +454,7 @@ def test_read_hole(session: Session, client: TestClient):
     session.commit()
 
     response = client.get(f"/courses/holes/{hole.id}")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert data["number"] == hole.number
@@ -696,7 +471,7 @@ def test_update_hole(session: Session, client: TestClient):
     session.commit()
 
     response = client.patch(f"/courses/holes/{hole.id}", json={"number": 3})
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     data = response.json()
     assert data["number"] == 3
@@ -705,18 +480,6 @@ def test_update_hole(session: Session, client: TestClient):
     assert data["stroke_index"] == hole.stroke_index
     assert data["tee_id"] == hole.tee_id
     assert data["id"] == hole.id
-
-
-def test_delete_hole(session: Session, client: TestClient):
-    hole = Hole(number=1, par=4, yardage=385, stroke_index=3, tee_id=1)
-    session.add(hole)
-    session.commit()
-
-    response = client.delete(f"/courses/holes/{hole.id}")
-    assert response.status_code == 200
-
-    hole_db = session.get(Hole, hole.id)
-    assert hole_db is None
 
 
 def test_read_course_with_data(session: Session, client: TestClient):
@@ -736,7 +499,7 @@ def test_read_course_with_data(session: Session, client: TestClient):
 
     tee = Tee(
         name="Test Tee",
-        gender="M",
+        gender=TeeGender.MENS,
         rating=72.3,
         slope=128,
         color="Blue",
@@ -761,7 +524,7 @@ def test_read_course_with_data(session: Session, client: TestClient):
     session.commit()
 
     response = client.get(f"/courses/{course.id}")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
 
     course_data = response.json()
     assert course_data["name"] == course.name
