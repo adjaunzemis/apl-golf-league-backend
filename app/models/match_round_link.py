b00489a028a497e67df7767a824993bd691060c6
Andris Jaunzemis <adjaunzemis@gmail.com>
2025-04-27 11:50:40 -0400

    Add team id to match round link model

--- app/models/match_round_link.py
+++ app/models/match_round_link.py
@@ -2,5 +2,6 @@ from sqlmodel import Field, SQLModel
 
 
 class MatchRoundLink(SQLModel, table=True):
-    match_id: int = Field(default=None, foreign_key="match.id", primary_key=True)
-    round_id: int = Field(default=None, foreign_key="round.id", primary_key=True)
+    match_id: int = Field(..., foreign_key="match.id", primary_key=True)
+    round_id: int = Field(..., foreign_key="round.id", primary_key=True)
+    team_id: int = Field(..., foreign_key="team.id")
